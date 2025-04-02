7d365bc57f39d9e84143b390c01dc081caf59fc3
jun3181 <rlatjdwns0109@gmail.com>
2025-04-02 19:53:11 +0900

    ㅁㄹ겟다

--- 내공부/Project1/Project1/pratice.cpp
+++ 내공부/Project1/Project1/pratice.cpp
@@ -1,14 +1,31 @@
 ﻿#include <iostream>
-#include <string>
-#include <cstdlib>  // rand(), srand()
-#include <ctime>    // time()
-#include <cstdlib>  // abs()
-
 
 using namespace std;
+
 int main() {
+	int arr[5] = { 5,6,2,8,1 };
+	int n = arr[0];
+	int i = 0;
+	int temp = 0;
+	while (true) {
+		int j = i;
+		temp = n;
+		for (j; j < 5; j++) {
+			if (n > arr[j]) {
+				n = arr[j];
 
+			}
+		}
+		arr[i] = n;
+		i++;
+		n = arr[i];
+		if (i > 5) {
+			break;
+		}
 
+	}
+	for (int j = 0; j < 5; j++) {
+		cout << arr[j] << " ";
+		}
 }
-
-
+	
