482a5799c82a541b68b2c1e99207e8d0f8ac2097
jun3181 <rlatjdwns0109@gmail.com>
2025-04-10 05:51:33 +0900

    ㅗㅗㅗ

--- 내공부/Project1/Project1/pratice.cpp
+++ 내공부/Project1/Project1/pratice.cpp
@@ -1,18 +1,14 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
-const int MAX = 1000;
-int main() {
-	ifstream infile("input34.txt");
-	string lines[MAX];
-	string line;
-	int n = 0;
-	while (getline(infile, line))
-		lines[n++] = line;
+#include <cctype>
 
-	infile.close();
-	for (int i = 0; i < n; i++)
-		cout << lines[i] << endl;
-	return 0;
+using namespace std;
+
+const int MAX = 1000;
+
+int main() {
+
+
+
 }
---
+++ 내공부/두번째 과제/prob3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+const int MAX = 1000;
+
+int main() {
+    string words[MAX];   // 고유한 단어 저장
+    int count[MAX] = { 0 }; // 단어별 개수
+    int wordcount = 0;   // 고유 단어 개수
+
+    string word;
+    ifstream infile("input34.txt");
+
+    if (!infile) {
+        cout << "파일을 열 수 없습니다!" << endl;
+        return 1;
+    }
+
+    while (infile >> word) {
+        // 단어를 소문자로 변환
+        for (char& ch : word) {
+            ch = tolower(ch);
+        }
+
+        // 단어가 이미 존재하는지 확인
+        bool found = false;
+        for (int i = 0; i < wordcount; i++) {
+            if (words[i] == word) {
+                count[i]++;
+                found = true;
+                break;
+            }
+        }
+
+        // 새 단어라면 추가
+        if (!found) {
+            words[wordcount] = word;
+            count[wordcount] = 1;
+            wordcount++;
+        }
+    }
+
+    infile.close();
+
+    // 결과 출력
+    for (int i = 0; i < wordcount; i++) {
+        cout << words[i] << " " << count[i] << endl;
+    }
+
+    return 0;
+}
