55fdcd8e31f1639e9e98f9a363dc41eb67ada41a
jun3181 <rlatjdwns0109@gmail.com>
2025-04-14 01:48:21 +0900

    f

--- 내공부/Project1/Project1/pratice.cpp
+++ 내공부/Project1/Project1/pratice.cpp
@@ -1,12 +1,34 @@
 ﻿#include <iostream>
 #include <string>
-#include <vector>
-#include <fstream>
-#include <algorithm>
-
 
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
 
+    string commonPrefix;
+    cin >> commonPrefix;
+
+    for (int i = 1; i < n; ++i) {
+        string word;
+        cin >> word;
+
+        int minLength = min(commonPrefix.length(), word.length());
+        int j = 0;
+
+        while (j < minLength && commonPrefix[j] == word[j]) {
+            ++j;
+        }
+
+        commonPrefix = commonPrefix.substr(0, j);
+
+        if (commonPrefix.empty()) {
+            break;
+        }
+    }
+
+    cout << commonPrefix << endl;
+
+    return 0;
 }
