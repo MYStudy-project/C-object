945f6074bdb75540d9828f122ce1b7cee49329a4
jun3181 <rlatjdwns0109@gmail.com>
2025-04-15 17:19:41 +0900

    ddd

--- 내공부/Project1/Project1/pratice.cpp
+++ 내공부/Project1/Project1/pratice.cpp
@@ -1,23 +1,6 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-
-
-int main() {
-	int n,a;
-	int count = 0;
-	cin >> n;
-	vector<int> arr;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		count++;
-		for (int j = 0; j < count; j++) {
-			if (a != arr[j]) {
-				arr.push_back(a);
-			}
-		}
-	}
-	cout << arr.size();
-}
