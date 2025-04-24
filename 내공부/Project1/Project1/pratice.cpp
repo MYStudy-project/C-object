b01fd5b768f6efa7e6c5135064b6a538e2c02aee
jun3181 <rlatjdwns0109@gmail.com>
2025-04-25 06:31:49 +0900

    dfdf

--- 내공부/Project1/Project1/pratice.cpp
+++ 내공부/Project1/Project1/pratice.cpp
@@ -1,55 +1,33 @@
 ﻿#include <iostream>
-#include <fstream>
+#include <string>
 #include <vector>
-#include <cmath>
 using namespace std;
 
-class Rectangle {
+class A {
 public:
-    int xmin, xmax, ymin, ymax;
+	int data;
+	A() {
+		cout << "constructor1 of A" << endl;
+	}
+	A(int a) : data(a) {
+		cout << "constructor2 of A" << endl;
+	}
 };
-
+class B {
+public:
+	vector<A> vec;
+	A z;
+	B() {
+		cout << "constructor1 of B" << endl;
+	}
+	B(vector<A> v) : vec(v) {
+		cout << "constructor 2 of B" << endl;
+	}
+};
 int main() {
-    ifstream file("input2.txt");
-    int N;
-    file >> N;
-
-    vector<Rectangle> rects(N);
-
-    int global_xmin = INT32_MAX, global_ymin = INT32_MAX;
-    int global_xmax = INT32_MIN, global_ymax = INT32_MIN;
-
-    for (int i = 0; i < N; ++i) {
-        file >> rects[i].xmin >> rects[i].xmax >> rects[i].ymin >> rects[i].ymax;
-        if (rects[i].xmin < global_xmin) global_xmin = rects[i].xmin;
-        if (rects[i].xmax > global_xmax) global_xmax = rects[i].xmax;
-        if (rects[i].ymin < global_ymin) global_ymin = rects[i].ymin;
-        if (rects[i].ymax > global_ymax) global_ymax = rects[i].ymax;
-    }
-
-    file.close();
-
-    double centerX = (global_xmin + global_xmax) / 2.0;
-    double centerY = (global_ymin + global_ymax) / 2.0;
-
-    double max_dist = 0;
-    vector<pair<int, int>> corners = {
-        {global_xmin, global_ymin},
-        {global_xmin, global_ymax},
-        {global_xmax, global_ymin},
-        {global_xmax, global_ymax}
-    };
-
-    for (auto p : corners) {
-        double dx = centerX - p.first;
-        double dy = centerY - p.second;
-        double dist = sqrt(dx * dx + dy * dy);
-        if (dist > max_dist)
-            max_dist = dist;
-    }
-
-    cout << "Center: (" << centerX << ", " << centerY << ")" << endl;
-    cout << "Radius: " << max_dist << endl;
-
-    return 0;
+	A x(10);
+	vector<A> vec_a;
+	A arr[4];
+	B y(vec_a);
+	return 0;
 }
